1698e72cc9ad5f2e7330785188ee6a504ebd9782
Bambo0k <95869835+Bambo0k@users.noreply.github.com>
2022-08-11 02:14:57 +0300

    Linter fix

--- Weather_widget.js
+++ Weather_widget.js
@@ -1,11 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-expressions */
+/* eslint-disable no-sequences */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
 fetch('https://api.weatherapi.com/v1/current.json?key=b677082ac19f45aca07212645220401&q=Kiev&aqi=no')
     .then((response) => response.json())
+    // eslint-disable-next-line no-use-before-define
     .then((data) => render(data));
 
 const render = (data) => {
-    document.querySelector(".location").textContent = data.location.name,
-        document.querySelector(".temp").innerHTML = data.current.temp_c,
-        document.querySelector(".img").src = `https://${data.current.     condition.icon}`,
+    document.querySelector('.location').textContent = data.location.name,
+        document.querySelector('.temp').innerHTML = data.current.temp_c,
+        document.querySelector('.img').src = `https://${data.current.condition.icon}`,
         document.querySelector('.humidity').textContent = data.current.cloud,
         document.querySelector('.wind').textContent = data.current.wind_mph;
-}
+};
